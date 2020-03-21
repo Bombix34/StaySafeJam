9d8872b050b3a9e6ff72401e91bc1cb81eb2259d
Hugo <bombix34@gmail.com>
2020-03-21 22:31:09 +0100

    tite base trankil

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            Vector2 test = Event.current.mousePosition;
+            this.transform.position = test;
+        }
+    }
+}
