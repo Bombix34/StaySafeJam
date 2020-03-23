7cb36ebb91e50e386527ff00166e86474c645498
Simon <simongirau@gmail.com>
2020-03-23 18:42:49 +0100

    tweaking

--- Assets/Scripts/BackgroundFX.cs
+++ Assets/Scripts/BackgroundFX.cs
@@ -8,6 +8,7 @@ public class BackgroundFX : MonoBehaviour
     public int gradientState = 5;
     public int fuckedColors = 10;
     public float multiplyColor = 2f;
+    public float maxColor = 0.85f;
     public float multiplyFX = 0.05f;
 
     int nbrEat;
@@ -20,11 +21,13 @@ public class BackgroundFX : MonoBehaviour
         material = background.GetComponent<SpriteRenderer>().material;
         colors = 0.4f;
         colorBackground = 0;
+        material.SetColor("_Color", new Color(colorBackground, colorBackground, colorBackground));
     }
 
     public void Background()
     {
         colorBackground += multiplyColor;
+        colorBackground = Mathf.Clamp(colorBackground, 0, maxColor);
         material.SetColor("_Color", new Color(colorBackground, colorBackground, colorBackground));
 
         nbrEat += 1;
